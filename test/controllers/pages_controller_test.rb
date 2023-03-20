6ce6bf18ad6c9ae088afb92dccf66277279df629
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-03-20 19:57:57 -0400

    set up api key and starting pages

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get pages_index_url
+    assert_response :success
+  end
+
+  test "should get test" do
+    get pages_test_url
+    assert_response :success
+  end
+
+  test "should get search" do
+    get pages_search_url
+    assert_response :success
+  end
+end
